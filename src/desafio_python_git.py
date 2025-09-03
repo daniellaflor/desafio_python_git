ac9bfbe0e6c425531d54245726f33e24f7c4ffca
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2025-09-03 17:52:04 -0300

    Adicionei os anos e fiz o calculo

--- src/desafio_python_git.py
+++ src/desafio_python_git.py
@@ -2,3 +2,8 @@ nome=str(input('Escreva seu nome: '))
 idade=int(input('Escreva sua idade: '))
 programacaoFavorita=str(input('Escreva sua programação favorita: '))
 print(f'Seu nome é: {nome}\n Sua idade é: {idade}\n Sua programação favorita é: {programacaoFavorita}')
+
+ano_atual=2025
+nascimento=2009
+calculo=( ano_atual - nascimento)
+print(f"Ela nasceu no ano: {nascimento}\n O ano atual é: {ano_atual}\n Sua idade é: {calculo}")
