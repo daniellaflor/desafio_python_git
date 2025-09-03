7524ea4800ce3465176f1180ff0137db9a717c76
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2025-09-03 17:29:55 -0300

    primeiro_commit

---
+++ desafio_python_git.py
@@ -0,0 +1,4 @@
+nome=str(input('Escreva seu nome: '))
+idade=int(input('Escreva sua idade: '))
+programacaoFavorita=str(input('Escreva sua programação favorita: '))
+print(f'Seu nome é: {nome}\n Sua idade é: {idade}\n Sua programação favorita é: {programacaoFavorita}')
